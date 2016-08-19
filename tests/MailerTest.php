dc981486c6e2115e26f8bdfc87be5f9ca7d55c40
Pierre Rudloff <contact@rudloff.pro>
2016-08-19 19:57:29 +0200

    Mock MW api in test

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -11,9 +11,26 @@ class MailerTest extends \PHPUnit_Framework_TestCase
 
     public function testSend()
     {
-        $climate = $this->createMock('League\CLImate\CLImate');
-        $climate->arguments = $this->createMock('League\CLImate\Argument\Manager');
-        $climate->arguments->method('get')->willReturn(true);
+        $api = $this->createMock('Mediawiki\Api\MediawikiApi');
+        $mailer = new Mailer(
+            $api,
+            'emailuser',
+            $logger = new Logger(new CLImate())
+        );
+        $this->assertTrue(
+            $mailer->send(
+                'Rudloff',
+                'Et eveniet qui rem sed omnis voluptatem aut. A praesentium nam numquam impedit ipsam est est. '.
+                'Voluptatem et esse quia ipsa et voluptatem nemo facilis. '.
+                'Enim eaque ullam est doloribus officia minus non. '.
+                'Minima ut explicabo quae deleniti autem occaecati.',
+                'Test'
+            )
+        );
+    }
+
+    public function testSendWithRealApi()
+    {
         $mailer = new Mailer(
             MediawikiApi::newFromApiEndpoint('https://fr.wikipedia.org/w/api.php'),
             'emailuser',
